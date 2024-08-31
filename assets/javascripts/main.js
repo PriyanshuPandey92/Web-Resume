dccbc4d0c5915ff22a2ce479f6087bc23999fe83
Priyanshu Pandey <72819844+PriyanshuPandey92@users.noreply.github.com>
2024-08-31 16:00:48 +0530

    Add files via upload

---
+++ assets/javascripts/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#main-nav a").click(function(){
+        $("section").removeClass("show");
+        var target = $(this).attr("href");
+        $(target).addClass("show");
+    })
+});
